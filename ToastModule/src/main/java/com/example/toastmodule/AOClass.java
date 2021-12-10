3ab7440be3bf5a8c0f8e593e0e6f153af07103f9
MSufyanHafeez <sufyanhafeez77.com>
2021-12-10 16:41:16 +0500

    (second try)fourth commit with seperate java class for arithmetic operations

---
+++ ToastModule/src/main/java/com/example/toastmodule/AOClass.java
@@ -0,0 +1,16 @@
+package com.example.toastmodule;
+
+public class AOClass {
+    public static double sumFun(double a, double b){
+        return a+b;
+    }
+    public static double subFun(double a, double b){
+        return a-b;
+    }
+    public static double multiplyFun(double a, double b){
+        return a*b;
+    }
+    public static double divideFun(double a, double b){
+        return a/b;
+    }
+}
