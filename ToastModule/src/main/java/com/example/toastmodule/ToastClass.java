74fd7df886f514b429d5f86b627ee89d76663f6a
MSufyanHafeez <sufyanhafeez77.com>
2021-12-10 16:33:01 +0500

    fourth commit with seperate java class for arithmetic operations

--- ToastModule/src/main/java/com/example/toastmodule/ToastClass.java
+++ ToastModule/src/main/java/com/example/toastmodule/ToastClass.java
@@ -7,16 +7,4 @@ public class ToastClass {
     public static void simpletoast(Context context, String message){
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
-    public static double sumFun(double a, double b){
-        return a+b;
-    }
-    public static double subFun(double a, double b){
-        return a-b;
-    }
-    public static double multiplyFun(double a, double b){
-        return a*b;
-    }
-    public static double divideFun(double a, double b){
-        return a/b;
-    }
 }
